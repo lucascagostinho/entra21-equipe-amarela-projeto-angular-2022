f08fbee0f3fab033c348cd755175ecf887d40b29
Alexsandro-Correa <exemplo@seuemail.com.br>
2022-09-08 17:44:01 -0300

    Atualização footer e header

--- Olimpic/src/app/header/header.component.ts
+++ Olimpic/src/app/header/header.component.ts
@@ -9,11 +9,36 @@ import { SegurancaService } from '../seguranca.service';
 })
 export class HeaderComponent implements OnInit {
 
+  links!: Array<any>
+
   constructor(private router: Router,
     private seguranca: SegurancaService) { }
 
   ngOnInit(): void {
     this.seguranca.entrou = false;
+
+    this.links = new Array();
+    this.links.push({
+      rota: "home",
+      textContent: "Home",
+    });
+    this.links.push({
+      rota: "faq",
+      textContent: "FAQs",
+    });
+    this.links.push({
+      rota: "about",
+      textContent: "About",
+    });
+    this.links.push({
+      rota: "login",
+      textContent: "Login",
+    });
+    this.links.push({
+      rota: "register",
+      textContent: "Register",
+    });
+
   }
 
 }
